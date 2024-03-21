22eb881efca37334fea7626ccd4f0ea18761c64e
RoyW12 <roywijaya893@gmail.com>
2024-03-21 16:26:47 +0700

    P4_Percobaan1

---
+++ src/P4/BruteForceDivideConquer/Faktorial.java
@@ -0,0 +1,23 @@
+package P4.BruteForceDivideConquer;
+
+public class Faktorial {
+    public int nilai;
+
+    int faktorialBF(int n) {
+        int fakto = 1;
+        for (int i = 0; i <= n; i++) {
+            fakto = fakto * i;
+        }
+
+        return fakto;
+    }
+
+    int faktorialDC(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            int fakto = n * faktorialDC(n - 1);
+            return fakto;
+        }
+    }
+}
