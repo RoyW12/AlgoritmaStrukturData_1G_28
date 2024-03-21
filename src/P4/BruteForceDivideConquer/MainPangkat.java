9f88514cd97168af6aa8a65447e149f9bb3353a2
RoyW12 <roywijaya893@gmail.com>
2024-03-21 16:34:39 +0700

    P4_Percobaan2_Pertanyaan4

--- src/P4/BruteForceDivideConquer/MainPangkat.java
+++ src/P4/BruteForceDivideConquer/MainPangkat.java
@@ -15,21 +15,33 @@ public class MainPangkat {
             int nilai = sc.nextInt();
             System.out.print("Masukkan nilai pemangkat: ");
             int pangkat = sc.nextInt();
-
             png[i] = new Pangkat(nilai, pangkat);
         }
 
-        System.out.println("HASIL PANGKAT - BRUTE FORCE");
-        for (int i = 0; i < elemen; i++) {
-            System.out.println("Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah "
-                    + png[i].pangkatBF(png[i].nilai, png[i].pangkat));
+        System.out.println("Pilih Metode: ");
+        System.out.println("1. Brute Force");
+        System.out.println("2. Divide and Conquer");
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1:
+                System.out.println("HASIL PANGKAT - BRUTE FORCE");
+                for (int i = 0; i < elemen; i++) {
+                    System.out.println("Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah "
+                            + png[i].pangkatBF(png[i].nilai, png[i].pangkat));
+                }
+                break;
+            case 2:
+                System.out.println("HASIL PANGKAT - DIVIDE AND CONQUER");
+                for (int i = 0; i < elemen; i++) {
+                    System.out.println("Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah "
+                            + png[i].pangkatBF(png[i].nilai, png[i].pangkat));
+                }
+                break;
+            default:
+                System.out.println("Pilihan salah");
+                System.out.println("Program berhenti");
+                break;
         }
 
-        System.out.println("HASIL PANGKAT - DIVIDE AND CONQUER");
-        for (int i = 0; i < elemen; i++) {
-            System.out.println("Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah "
-                    + png[i].pangkatBF(png[i].nilai, png[i].pangkat));
-
-        }
     }
 }
