7755fd0dd1a0a92b2e034c3198ad77b529ebe28a
RoyW12 <roywijaya893@gmail.com>
2024-04-30 18:34:32 +0700

    P10_Percobaan2

---
+++ src/P10/Praktikum2/Nasabah27.java
@@ -0,0 +1,22 @@
+package P10.Praktikum2;
+
+public class Nasabah27 {
+    String norek;
+    String nama;
+    String alamat;
+    int umur;
+    double saldo;
+
+    public Nasabah27() {
+
+    }
+
+    Nasabah27(String norek, String nama, String alamat, int umur, double saldo) {
+        this.norek = norek;
+        this.nama = nama;
+        this.alamat = alamat;
+        this.umur = umur;
+        this.saldo = saldo;
+
+    }
+}
---
+++ src/P10/Praktikum2/QueueMain.java
@@ -0,0 +1,62 @@
+package P10.Praktikum2;
+
+import java.util.Scanner;
+
+public class QueueMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan kapasitas queue: ");
+        int max = sc.nextInt();
+        Queue27 antri = new Queue27(max);
+        int pilih;
+
+        do {
+            menu();
+            pilih = sc.nextInt();
+
+            switch (pilih) {
+                case 1:
+                    sc.nextLine();
+                    System.out.print("Masukkan No Rekening: ");
+                    String norek = sc.nextLine();
+                    System.out.print("Masukkan nama: ");
+                    String nama = sc.nextLine();
+                    System.out.print("Masukkan alamat: ");
+                    String alamat = sc.nextLine();
+                    System.out.print("Masukkan umur: ");
+                    int umur = sc.nextInt();
+                    System.out.print("Masukkan saldo: ");
+                    double saldo = sc.nextDouble();
+                    Nasabah27 nb = new Nasabah27(norek, nama, alamat, umur, saldo);
+                    sc.nextLine();
+                    antri.Enqueue(nb);
+                    break;
+                case 2:
+                    Nasabah27 data = antri.Dequeue();
+                    if (!"".equals(data.norek) && !"".equals(data.nama) && !"".equals(data.alamat) && data.umur != 0
+                            && data.saldo != 0) {
+                        System.out.println("Antrian yang keluar: " + data.norek + " " + data.nama + " " + data.alamat
+                                + " " + data.umur + " " + data.saldo);
+                    }
+                    break;
+                case 3:
+                    antri.peek();
+                    break;
+                case 4:
+                    antri.print();
+                    break;
+
+            }
+        } while (pilih == 1 || pilih == 2 || pilih == 3 || pilih == 4);
+    }
+
+    public static void menu() {
+        System.out.println("Pilih menu: ");
+        System.out.println("1. Antrian Baru");
+        System.out.println("2. Antrian Keluar");
+        System.out.println("3. Cek Antrian Terdepan");
+        System.out.println("4. Cek Semua Antrian");
+        System.out.println("5. Cek Antrian Paling Belakang");
+        System.out.println("---------------------------");
+    }
+}
