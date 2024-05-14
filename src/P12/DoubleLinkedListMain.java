7e515caede9e748f9873493c455ed36d88b1ff9d
RoyW12 <roywijaya893@gmail.com>
2024-05-14 21:40:13 +0700

    P12_Percobaan3

--- src/P12/DoubleLinkedListMain.java
+++ src/P12/DoubleLinkedListMain.java
@@ -40,5 +40,22 @@ public class DoubleLinkedListMain {
         dll.print();
         System.out.println("Size : " + dll.size());
 
+        dll.print();
+        System.out.println("Size " + dll.size());
+        System.out.println("==============================");
+        dll.addFirst(3);
+        dll.addLast(4);
+        dll.addFirst(7);
+        dll.print();
+        System.out.println("Size: " + dll.size());
+        System.out.println("==============================");
+        dll.add(40, 1);
+        dll.print();
+        System.out.println("Size: " + dll.size());
+        System.out.println("==============================");
+        System.out.println("Data awal pada Linked Lists adalah: " + dll.getFirst());
+        System.out.println("Data akhir pada Linked Lists adalah: " + dll.getLast());
+        System.out.println("Data indeks ke-1 pada Linked Lists adalah: " + dll.get(1));
+
     }
 }
